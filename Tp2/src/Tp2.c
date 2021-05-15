5c24b0f7eb9442f50d9d0fb8d4b930f90f731167
Ramiro Olicino <60629180+ramirolicino@users.noreply.github.com>
2021-05-15 18:59:29 -0300

    Add files via upload

---
+++ Tp2/src/Tp2.c
@@ -0,0 +1,149 @@
+/*
+ ============================================================================
+ Name        : Tp2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "ArrayEmployees.h"
+
+
+#define TAM 1000
+
+
+int main()
+{
+	setbuf(stdout, NULL);
+
+    eEmployee listEmp[TAM];
+    int flag = 0;
+    int nextID = 1000;
+    int numberEmployees = 0;
+    char Continue = 'n';
+    char exit;
+
+
+    if(initEmployees(listEmp, TAM))
+    {
+        printf(" * INICIALIZACION CORRECTA * \n\n");
+    }
+    else
+    {
+        printf(" * INICIALIZACION ERRONEA * \n\n");
+    }
+
+    do
+    {
+    	//system("cls");
+
+        switch(menu())
+        {
+        case 1:
+            if(addEmployee(listEmp, TAM, &nextID))
+            {
+                printf(" * Successfully Added * \n");
+                numberEmployees++;
+                flag = 1;
+            }
+            else
+            {
+                printf(" * Wrong Added * \n");
+            }
+
+            break;
+
+        case 2:
+            if(!flag)
+            {
+                printf(" * PRIMERO DAR DE ALTA UN EMPLEADO * \n");
+            }
+            else
+            {
+                if(modifyEmployee(listEmp, TAM))
+                {
+                    printf(" * Successfully Modified * \n");
+                }
+                else
+                {
+                    printf(" * Wrong Modified * \n");
+                }
+
+            }
+            break;
+
+        case 3:
+            if(!flag)
+            {
+                printf(" * PRIMERO DAR DE ALTA UN EMPLEADO * \n");
+            }
+            else
+            {
+                if(removeEmployee(listEmp, TAM))
+                {
+                    printf(" * Successfully Removed * \n");
+                    numberEmployees--;
+                }
+                else
+                {
+                    printf(" * Wrong Removed * \n");
+                }
+            }
+
+            break;
+
+        case 4:
+            if(!flag)
+            {
+                printf(" * PRIMERO DAR DE ALTA UN EMPLEADO * \n");
+            }
+            else
+            {
+                if(totalAndAverage(listEmp, TAM, numberEmployees))
+                {
+                    printf("OK \n");
+                    printf("%d \n\n", numberEmployees);
+                }
+                else
+                {
+                    printf("NOT OK \n");
+                }
+
+            }
+            break;
+
+        case 5:
+            printf("EXIT?(Y/N)-> ");
+            fpurge(stdin);
+            scanf("%c", &exit);
+
+            if(exit == 'y')
+            {
+                Continue = 'y';
+            }
+
+            break;
+
+        default:
+            printf("Enter option correct...");
+            break;
+        }
+
+        //system("pause");
+
+    }
+    while(Continue == 'n');
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
